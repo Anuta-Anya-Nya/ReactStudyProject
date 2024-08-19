11591e3288b2c9136dfe0e94685be5d23eb0099b
Aeshe <syslichek@mail.ru>
2024-08-19 13:52:55 +0300

    add redux-thunk, robot message

--- src/components/ChatsComp.js
+++ src/components/ChatsComp.js
@@ -16,6 +16,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addChatAct, delChatAct } from "../store/chats/actionCreators";
 import { addMessage } from "../store/messages/actionCreators";
 import { getChatList } from "../store/chats/selectors";
+import { AUTHORS, AUTO_MESSAGE } from "../store/constants/messages";
 
 const theme = createTheme({
   palette: {
@@ -31,15 +32,14 @@ const theme = createTheme({
 function ChatsComp() {
   const [messageBody, setMessageBody] = useState({ text: "", author: "" });
   const [chatId, setChatId] = useState();
-  const ROBOT_MES = "Привет, человек! я получил твое сообщение";
+  // const ROBOT_MES = "Привет, человек! я получил твое сообщение";
 
   const chatList = useSelector(getChatList);
-  console.log("chatList", chatList);
 
   const messageList = useSelector((state) => state.message.messageList);
-  console.log("messageList", messageList);
 
   const dispatch = useDispatch();
+
   const selectChat = (chatId) => setChatId(chatId);
 
   const deleteChat = (chatId) => {
@@ -50,22 +50,39 @@ function ChatsComp() {
     dispatch(addChatAct(nameUser));
   };
   const onAddMessage = (message) => {
-    dispatch(addMessage(chatId, message));
+    dispatch(addMessageWithThunk(chatId, message));
+    // dispatch(addMessage(chatId, message));
   };
 
-  useEffect(() => {
-    const mes = { text: ROBOT_MES, author: "robot" };
-    if (chatId) {
-      if (
-        messageList[chatId].length > 0 &&
-        messageList[chatId].slice(-1)[0].author !== "robot"
-      ) {
-        setTimeout(() => {
-          onAddMessage(mes);
-        }, 1500);
-      }
+  const addMessageWithThunk = (chatId, message) => (dispatch, getState) => {
+    dispatch(addMessage(chatId, message));
+    if (message.author !== AUTHORS.BOT) {
+      setTimeout(
+        () =>
+          dispatch(
+            addMessage(chatId, {
+              text: AUTO_MESSAGE.HELLO_MES,
+              author: AUTHORS.BOT,
+            })
+          ),
+        2000
+      );
     }
-  }, [messageList]);
+  };
+
+  // useEffect(() => {
+  //   const mes = { text: ROBOT_MES, author: "robot" };
+  //   if (chatId) {
+  //     if (
+  //       messageList[chatId].length > 0 &&
+  //       messageList[chatId].slice(-1)[0].author !== "robot"
+  //     ) {
+  //       setTimeout(() => {
+  //         onAddMessage(mes);
+  //       }, 1500);
+  //     }
+  //   }
+  // }, [messageList]);
 
   return (
     <ThemeProvider theme={theme}>
---
+++ src/store/constants/messages.js
@@ -0,0 +1,7 @@
+export const AUTHORS = {
+  BOT: "robot",
+};
+
+export const AUTO_MESSAGE = {
+  HELLO_MES: "Привет, человек! я получил твое сообщение",
+};
